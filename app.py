6cd2f34f5aafae22f09c3a5759573dca0d245ea2
Apurva Singh <apurva.agra15@gmail.com>
2021-12-21 20:42:41 +0530

    database

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import os
 from flask import Flask, request
 import requests
 
-
 app = Flask(__name__)
 API_KEY = dict(os.environ)["API_KEY"]
 bot = telebot.TeleBot(API_KEY)
@@ -11,14 +10,40 @@ bot = telebot.TeleBot(API_KEY)
 
 def broadcast_messages(list_of_groups, msg):
     for group in list_of_groups:
-        to_url = f"https://api.telegram.org/bot{API_KEY}/sendMessage?chat_id={group}&text={msg}&parse_mode=HTML"
-        resp = requests.get(to_url)
+        to_url = f"https://api.telegram.org/bot{API_KEY}/sendMessage"
+        payload = {"chat_id": group, "text": msg, "parse_mode": "HTML"}
+        resp = requests.post(to_url, json=payload)
 
 
 def parse_message(msg):
+    print("type of msg:>", type(msg))
     chat_id = msg["message"]["chat"]["id"]
     txt = msg["message"]["text"]
-    return chat_id, txt
+    first_name = msg["message"]["chat"]["first_name"]
+    username = msg["message"]["chat"]["username"]
+
+    return chat_id, txt, first_name, username
+
+
+url = "https://e-commerce-api-apurva.herokuapp.com/api/v1/telebot"
+
+
+def broadcastToAll(msg):
+    usersURL = f"{url}/getAllUsers"
+    allUsers = requests.get(usersURL).json()
+    allUsersList = []
+    for user in allUsers["telegramUsers"]:
+        allUsersList.append(user["chat_id"])
+    broadcast_messages(allUsersList, msg)
+
+
+def addToDatabase(chat_id, username, first_name):
+    registerURL = f"{url}/register"
+
+    payload = {"chat_id": chat_id, "username": username, "first_name": first_name}
+    resp = requests.post(registerURL, json=payload)
+    return resp
+
 
 def is_command(txt):
     return txt[0] == "/"
@@ -27,15 +52,9 @@ def is_command(txt):
 def execute_command(command, chat_id):
     if command == "/ipo":
         broadcast_messages([chat_id], "HI, welcome in world of IPOs..")
-    elif command == "/start":
-        broadcast_messages([chat_id], "Thanks for subscribing my service.")
     elif command == "/help":
-        help_msg = """
-        Following are the commands which you can use:
-        /help
-        ipo <name of company>
-        mf <category of mutual fund>
-        """
+        help_msg = "<pre>Following are the options:</pre>"
+        print(help_msg)
         broadcast_messages([chat_id], help_msg)
     else:
         broadcast_messages([chat_id], "No such command exists..")
@@ -45,6 +64,7 @@ def execute_command(command, chat_id):
 def hello_world():
     bot.remove_webhook()
     bot.set_webhook(url="https://flask-app-pxeg.onrender.com/" + API_KEY)
+    # bot.set_webhook(url="https://4f8f-106-214-77-65.ngrok.io/" + API_KEY)
     return "Hello, World!"
 
 
@@ -54,11 +74,23 @@ def hello_appu():
     return "Hello Apurva"
 
 
+@app.route("/all")
+def broadcasttousers():
+    broadcastToAll("Hi everone..")
+    return "Hi to all"
+
+
 @app.route("/" + API_KEY, methods=["POST"])
 def getMessage():
     msg = request.get_json()
-    chat_id, txt = parse_message(msg)
-    if is_command(txt):
+    chat_id, txt, first_name, username = parse_message(msg)
+    print(chat_id, first_name, username, txt)
+    if txt == "/start":
+        print(">>start<<")
+        response = addToDatabase(chat_id, username, first_name).json()
+        print(response)
+        broadcast_messages([chat_id], "Thanks for subscribing my service.")
+    elif is_command(txt):
         execute_command(txt, chat_id)
     else:
         broadcast_messages([chat_id], txt)
