4a542e537adcd4b0edb5d8f1c4508bd46c8a5147
Apurva Singh <apurva.agra15@gmail.com>
2021-12-22 21:45:30 +0530

    docs support added

--- app.py
+++ app.py
@@ -149,5 +149,6 @@ def getMessage():
     return "!", 200
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+if dict(os.environ)["ENVIRON"] == "DEV":
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=5000)
