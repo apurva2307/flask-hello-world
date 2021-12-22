bb0af82486629dbd52717b7086206e3d67712d94
Apurva Singh <apurva.agra15@gmail.com>
2021-12-22 10:59:47 +0530

    some improvement

--- app.py
+++ app.py
@@ -116,5 +116,5 @@ def getMessage():
     return "!", 200
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+# if __name__ == "__main__":
+#     app.run(host="0.0.0.0", port=5000)
