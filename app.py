6f87123956f421cfe5eae99c659158cf284e5527
Apurva Singh <apurva.agra15@gmail.com>
2021-12-22 10:59:06 +0530

    some improvement

--- app.py
+++ app.py
@@ -1,45 +1,40 @@
-import telebot
 import os
 from flask import Flask, request
 import requests
+import json
+
 
 app = Flask(__name__)
 API_KEY = dict(os.environ)["API_KEY"]
-bot = telebot.TeleBot(API_KEY)
+API_URL = f"https://api.telegram.org/bot{API_KEY}"
+data_url = "https://e-commerce-api-apurva.herokuapp.com/api/v1/telebot"
 
 
 def broadcast_messages(list_of_groups, msg):
     for group in list_of_groups:
-        to_url = f"https://api.telegram.org/bot{API_KEY}/sendMessage"
+        to_url = f"{API_URL}/sendMessage"
         payload = {"chat_id": group, "text": msg, "parse_mode": "HTML"}
         resp = requests.post(to_url, json=payload)
+        return resp
 
 
 def parse_message(msg):
-    print("type of msg:>", type(msg))
     chat_id = msg["message"]["chat"]["id"]
-    txt = msg["message"]["text"]
+    txt = msg["message"]["text"].lower()
     first_name = msg["message"]["chat"]["first_name"]
     username = msg["message"]["chat"]["username"]
-
     return chat_id, txt, first_name, username
 
 
-url = "https://e-commerce-api-apurva.herokuapp.com/api/v1/telebot"
-
-
 def broadcastToAll(msg):
-    usersURL = f"{url}/getAllUsers"
+    usersURL = f"{data_url}/getAllUsers"
     allUsers = requests.get(usersURL).json()
-    allUsersList = []
     for user in allUsers["telegramUsers"]:
-        allUsersList.append(user["chat_id"])
-    broadcast_messages(allUsersList, msg)
+        broadcast_messages([user["chat_id"]], msg)
 
 
 def addToDatabase(chat_id, username, first_name):
-    registerURL = f"{url}/register"
-
+    registerURL = f"{data_url}/register"
     payload = {"chat_id": chat_id, "username": username, "first_name": first_name}
     resp = requests.post(registerURL, json=payload)
     return resp
@@ -56,15 +51,45 @@ def execute_command(command, chat_id):
         help_msg = "<pre>Following are the options:</pre>"
         print(help_msg)
         broadcast_messages([chat_id], help_msg)
+    elif command[:5] == "/all " and chat_id == 44114772:
+        help_msg = command[5:]
+        print(help_msg)
+        broadcastToAll(help_msg)
     else:
         broadcast_messages([chat_id], "No such command exists..")
 
 
+@app.route("/set")
+def set_webhook():
+    url = dict(os.environ)["WEB_URL"]
+    webhook_url = f"{url}/{API_KEY}"
+    setWebhook = f"{API_URL}/setWebhook"
+    options = {
+        "url": webhook_url,
+        "allowed_updates": ["message"],
+        "drop_pending_updates": True,
+    }
+    requests.post(setWebhook, json=options)
+    return "Webhook has been set."
+
+
+@app.route("/clear")
+def delete_webhook():
+    deleteWebhook = f"{API_URL}/deleteWebhook"
+    options = {"drop_pending_updates": True}
+    requests.post(deleteWebhook, json=options)
+    return "Webhook has been removed."
+
+
+@app.route("/get")
+def get_webhook_info():
+    getWebhookInfo = f"{API_URL}/getWebhookInfo"
+    resp = requests.get(getWebhookInfo)
+    return json.dumps(resp.json()["result"])
+
+
 @app.route("/")
 def hello_world():
-    bot.remove_webhook()
-    bot.set_webhook(url="https://flask-app-pxeg.onrender.com/" + API_KEY)
-    # bot.set_webhook(url="https://4f8f-106-214-77-65.ngrok.io/" + API_KEY)
     return "Hello, World!"
 
 
@@ -74,21 +99,15 @@ def hello_appu():
     return "Hello Apurva"
 
 
-@app.route("/all")
-def broadcasttousers():
-    broadcastToAll("Hi everone..")
-    return "Hi to all"
-
-
 @app.route("/" + API_KEY, methods=["POST"])
 def getMessage():
     msg = request.get_json()
     chat_id, txt, first_name, username = parse_message(msg)
-    print(chat_id, first_name, username, txt)
-    if txt == "/start":
-        print(">>start<<")
+    if txt == "/start" or txt == "/subscribe":
         response = addToDatabase(chat_id, username, first_name).json()
-        print(response)
+        broadcast_messages(["44114772"], json.dumps(response))
+        broadcast_messages(["44114772"], chat_id)
+        broadcast_messages(["44114772"], username)
         broadcast_messages([chat_id], "Thanks for subscribing my service.")
     elif is_command(txt):
         execute_command(txt, chat_id)
@@ -97,5 +116,5 @@ def getMessage():
     return "!", 200
 
 
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
